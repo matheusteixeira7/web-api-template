c3e790abf870807ad294d75fb59b86579504d74d
Matheus <matheus.teixeira7@gmail.com>
2025-12-06 15:17:16 -0300

    chore: add react query

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -1,9 +1,14 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { getQueryClient } from "@/lib/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import * as React from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+
   return (
     <NextThemesProvider
       attribute="class"
@@ -12,7 +17,10 @@ export function Providers({ children }: { children: React.ReactNode }) {
       disableTransitionOnChange
       enableColorScheme
     >
-      {children}
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </NextThemesProvider>
-  )
+  );
 }
